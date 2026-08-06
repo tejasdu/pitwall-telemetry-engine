050d0b4597b04e38ecefdfe4050b994f2bbd5342
tejasdu <tejasdu@umich.edu>
2026-08-05 20:46:30 -0400

    Playground

--- src/pitwall_telemetry_engine/__init__.py
+++ src/pitwall_telemetry_engine/__init__.py
@@ -1,5 +1,17 @@
+import requests
+from requests.exceptions import HTTPError
+import json
+import pandas as pd
+from urllib.request import urlopen
+
 def main() -> None:
-    print("Hello from pitwall-telemetry-engine!")
+
+    response = urlopen('https://api.openf1.org/v1/car_data?driver_number=55&session_key=9159&speed>=315')
+    data = json.loads(response.read().decode('utf-8'))
+    
+    #with open('car_data.json', 'w') as f:
+    #    json.dump(data, f, indent=4)
+
 
 if __name__ == "__main__":
     main()
