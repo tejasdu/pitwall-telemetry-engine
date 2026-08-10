d5d5990ff6529607cb46ccee2fe55984e9a2f181
tejasdu <tejasdu@umich.edu>
2026-08-09 23:08:15 -0400

    Experimented with API & Pydantic

--- src/pitwall_telemetry_engine/__init__.py
+++ src/pitwall_telemetry_engine/__init__.py
@@ -6,11 +6,7 @@ from urllib.request import urlopen
 
 def main() -> None:
 
-    response = urlopen('https://api.openf1.org/v1/car_data?driver_number=55&session_key=9159&speed>=315')
-    data = json.loads(response.read().decode('utf-8'))
-    
-    #with open('car_data.json', 'w') as f:
-    #    json.dump(data, f, indent=4)
+    return None
 
 
 if __name__ == "__main__":
